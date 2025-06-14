3b17bfa37098fed0cba22141f237e9f71bb8658b
mogaka <jamesoyondi23@gmail.com>
2025-06-14 10:43:45 +0300

    feat: add CartItem and CartContextType interfaces for cart management

---
+++ src/contexts/cart-context.types.ts
@@ -0,0 +1,22 @@
+import type { Product } from "../types/product";
+
+export interface CartItem {
+	id: string;
+	product: Product;
+	quantity: number;
+	addedAt: string;
+}
+
+export interface CartContextType {
+	cartItems: CartItem[];
+	isCartOpen: boolean;
+	openCart: () => void;
+	closeCart: () => void;
+	toggleCart: () => void;
+	addToCart: (product: Product, quantity?: number) => void;
+	removeFromCart: (itemId: string) => void;
+	updateQuantity: (itemId: string, quantity: number) => void;
+	clearCart: () => void;
+	cartTotal: number;
+	itemCount: number;
+}
